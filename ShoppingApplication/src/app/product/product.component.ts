c9b81df86fe4bad95255f5700eaffe98399f90da
Rakesh Neelarapu <rakesh_roy123@yahoo.com>
2017-10-22 18:38:53 -0400

    changes

---
+++ ShoppingApplication/src/app/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product'
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  productName: string = "LED TV";
+  products: Product[] = [
+    { id: 1, name: "SAMSUNG", price: 10000 },
+    { id: 2, name: "LG", price: 20000 },
+    { id: 3, name: "SONY", price: 25000 },
+    { id: 4, name: "INSIGMA", price: 30000 }
+  ];
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
